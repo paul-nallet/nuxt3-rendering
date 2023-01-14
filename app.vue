dc37751a95fdc5f5b252042a1ea261471b44763c
Paul Nallet <paul@copromatic.com>
2023-01-14 17:32:14 +0100

    client only

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <ClientOnly fallbackTag="span">
+      commentaires client
+      <template #fallback>
+        <div>commentaires fallback</div>
+      </template>
+    </ClientOnly>
     <NuxtWelcome />
   </div>
 </template>
