80dc4e9d242a70c7d41fa52656edb98e619e20fa
Paul Nallet <paul@copromatic.com>
2023-01-16 21:25:57 +0100

    static hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: 'service-worker',
-  },
   modules: [
     '@nuxtjs/supabase',
     '@nuxt/content',
---
+++ pages/blog/rouge.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>rouge</div>
+</template>
